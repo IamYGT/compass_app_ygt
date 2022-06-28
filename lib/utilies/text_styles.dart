57ac13a1e0c916078e555fe1df4f5f5957eade4a
iamygt <ercanyter@gmail.com>
2022-06-28 10:10:44 +0300

    add compass ui

---
+++ lib/utilies/text_styles.dart
@@ -0,0 +1,10 @@
+import 'package:compass_app_ygt/utilies/sizes.dart';
+import 'package:compass_app_ygt/utilies/styles.dart';
+import 'package:flutter/widgets.dart';
+
+class TextStylesYGT {
+  static const columnHeadingTextStyle = TextStyle(
+      color: ColorsYGT.white,
+      fontSize: FontSizesYGT.fontSize26,
+      fontWeight: FontWeightYGT.bold);
+}
